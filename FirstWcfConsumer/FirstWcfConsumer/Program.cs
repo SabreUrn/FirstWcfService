692a4a43b8ee626876d8837de007f4be5cd5abbf
Kasp5953 <kasp5953@edu.easj.dk>
2018-02-12 11:09:15 +0100

    moves projects into subfolders

---
+++ FirstWcfConsumer/FirstWcfConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWcfConsumer {
+    class Program {
+        static void Main(string[] args) {
+            using(SayMyNameService.Service1Client client = new SayMyNameService.Service1Client()) {
+                Console.WriteLine(client.SayMyName());
+
+                Console.Read();
+            }
+        }
+    }
+}
